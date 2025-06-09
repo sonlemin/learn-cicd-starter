e0c42dd205580b83a86903dfe9facb3f01186adb
sonlemin <sonlm2005@icloud.com>
2025-06-09 15:32:52 +0700

    233

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -13,11 +13,11 @@ func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
-	}
+
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", error.New("malformed authorization header")
+		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[0], nil
+	return splitAuth[1], nil
 }
